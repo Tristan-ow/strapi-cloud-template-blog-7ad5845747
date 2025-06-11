2b24f718f361c7142a4d4f2c4bc3a4f4492b4897
Tristan Wegner <tristan@radventure.de>
2025-06-11 07:32:31 +0200

    Update shortcodeService.js

--- src/services/shortcodeService.js
+++ src/services/shortcodeService.js
@@ -7,7 +7,7 @@ const shortcodeService = async (data) => {
         kanzleialter: (new Date().getFullYear() - 1987),
     };
 
-    const fieldsToProcess = ["content", "description", "body", "text", "heading", "headerInhalt"]; // Specify fields to replace shortcodes in
+    const fieldsToProcess = ["content", "description", "body", "text", "heading", "headerInhalt", "Badge1"]; // Specify fields to replace shortcodes in
 
     if (typeof data === "object" && data !== null) {
         for (const key in data) {
