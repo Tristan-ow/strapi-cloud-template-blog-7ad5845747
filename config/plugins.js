f8a8f1c54ff1444d47afca92e07b002d92b50be0
Tristan Wegner <tristan@radventure.de>
2025-06-17 20:51:51 +0200

    add webp

--- config/plugins.js
+++ config/plugins.js
@@ -3,5 +3,15 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  'webp-converter': {
+    enabled: true,
+    config: {
+      // mimeTypes that converts to WebP. Default is ['image/png', 'image/jpeg', 'image/jpg']
+      mimeTypes: undefined,
+      options: {
+        // WebP options: https://sharp.pixelplumbing.com/api-output#webp
+      },
+    },
+  },
   // ...
 });
