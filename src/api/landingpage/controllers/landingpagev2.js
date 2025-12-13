5e3590351961f92f947ee6bc39bf8eaacc70e5db
Tristan Wegner <tristan@radventure.de>
2025-12-13 21:55:49 +0100

    Create landingpagev2.js

---
+++ src/api/landingpage/controllers/landingpagev2.js
@@ -0,0 +1,53 @@
+// src/api/landingpage/controllers/landingpage.js
+
+'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::landingpage-v2.landingpage-v2', ({ strapi }) => ({
+    async find(ctx) {
+        const landingpages = await strapi.entityService.findMany('api::landingpage-v2.landingpage-v2', {
+            populate: {
+                Content: { populate: '*' },
+                Hero: { populate: '*' },
+                Seo: { populate: '*' },
+            },
+            filters: ctx.query.filters || {}, // Filter übernehmen
+        });
+
+        const enhancedData = await Promise.all(
+            landingpages.map(async (entry) => {
+                const content = entry.Content || [];
+
+                const updatedContent = await Promise.all(
+                    content.map(async (component) => {
+                        if (
+                            component.__component === 'shared.faq' &&
+                            component.partial_faq?.id
+                        ) {
+                            const relatedFaq = await strapi.entityService.findOne(
+                                'api::partial-faq.partial-faq', // ggf. anpassen
+                                component.partial_faq.id,
+                                { populate: '*' }
+                            );
+
+                            return {
+                                ...component,
+                                partial_faq: relatedFaq,
+                            };
+                        }
+
+                        return component;
+                    })
+                );
+
+                return {
+                    ...entry,
+                    Content: updatedContent,
+                };
+            })
+        );
+
+        return { data: enhancedData };
+    },
+}));
